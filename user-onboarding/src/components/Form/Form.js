e66473123894df41a2cb1edd82b1b6d1307d4533
Darren Tebo <darrentebo83@outlook.com>
2020-06-09 16:41:42 -0400

    updated global html font size and styled the form

--- user-onboarding/src/components/Form/Form.js
+++ user-onboarding/src/components/Form/Form.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 
 import axios from 'axios';
 
+import './Form.css';
+
 const Form = () => {
     const [post, setPost] = useState();
 
@@ -12,9 +14,37 @@ const Form = () => {
         terms: false
     });
 
-    return (
-        <div className="form">
+    const [buttonDisabled, setButtonDisabled] = useState(true);
 
+    const handleChanges = (event) => {
+
+    };
+
+    const handleSubmit = (event) => {
+
+    };
+
+    return (
+        <div className="form-wrapper">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">
+                    Name
+                    <input type="text" id="name" name="name" value={formState.name} onChange={handleChanges} />
+                </label>
+                <label htmlFor="email">
+                    Email
+                    <input type="text" id="email" name="email" value={formState.email} onChange={handleChanges} />
+                </label>
+                <label htmlFor="password">
+                    Password
+                    <input type="text" id="password" name="password" value={formState.password} onChange={handleChanges} />
+                </label>
+                <label htmlFor="terms" className="terms">
+                    <input type="checkbox" id="terms" name="terms" checked={formState.terms} onChange={handleChanges} />
+                    Terms of Service
+                </label>
+                <button type="submit" disabled={buttonDisabled}>Submit</button>
+            </form>
         </div>
     );
 };
