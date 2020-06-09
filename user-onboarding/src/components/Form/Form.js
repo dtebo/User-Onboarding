a386c89846dcf6c96820fde54b6c7d2fa18c8708
Darren Tebo <darrentebo83@outlook.com>
2020-06-09 16:12:30 -0400

    added form state and post state

--- user-onboarding/src/components/Form/Form.js
+++ user-onboarding/src/components/Form/Form.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+import axios from 'axios';
 
 const Form = () => {
+    const [post, setPost] = useState();
+
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        password: "",
+        terms: false
+    });
+
     return (
         <div className="form">
 
